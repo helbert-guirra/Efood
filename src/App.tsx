801b9b51c2783e21f35a3a3358ee4745b06f36d5
HelbertGuirra <helbert.guirra@gmail.com>
2025-08-30 19:26:16 -0300

    Initial commit - efood project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import GlobalStyle from './styles'
+import Footer from './Components/Footer'
+
+import Rotas from './routes'
+import Cart from './Components/Cart'
+import { store } from './store'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <GlobalStyle></GlobalStyle>
+          <Rotas />
+          <Footer></Footer>
+          <Cart></Cart>
+        </div>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
